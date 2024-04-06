1389de18b1c8c75a239c99a3a9d2da4ca44f3861
Cypro Freelance <130126488+CyproFreelance@users.noreply.github.com>
2024-04-07 02:39:26 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   publicRoutes: ['/', '/contact' , '/about'],
+  ignoredRoutes: ['/']
 });
 
 export const config = {
