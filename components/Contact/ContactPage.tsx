2eb5b9e5cf86daeb0f70b2bc2ff7e2f021266148
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2024-04-13 01:37:17 +0530

    made the contact page responsive!

--- components/Contact/ContactPage.tsx
+++ components/Contact/ContactPage.tsx
@@ -1,18 +1,66 @@
 import React from "react";
+import "./Contact.css";
 
 const ContactPage = () => {
   return (
-    <div className="flex flex-col items-start mt-40 w-full ">
-      <div className="flex text-7xl">
-        <h1 className="bg-clip-text font-black text-transparent bg-gradient-to-r leading-tight mb-2 text-left from-[#ffffff] to-[rgba(255,255,255,0.42)] max-md:px-4">
-          HEY! Tell us all the things
-          <br />
-          you need
+    <div className="flex flex-col items-start mt-40 w-full contact">
+      <div className="box__heading__main flex text-7xl">
+        <h1 className="heading__main text-6xl bg-clip-text font-black text-transparent bg-gradient-to-r leading-tight mb-2 text-left from-[#ffffff] to-[rgba(255,255,255,0.42)]">
+          HEY! Tell us all the things you need
         </h1>
         <span>👋</span>
       </div>
 
-      <h3 className="text-[#FF4D00] text-xl">I'm interested in</h3>
+      <h3 className="text-[#FF4D00] text-xl subheading__main">
+        I'm interested in
+      </h3>
+
+      <div className="containerContact">
+        <form action="#">
+          <div className="form-row">
+            <div className="input-data">
+              <input type="text" required />
+              <div className="underline" />
+              <label htmlFor="">First Name</label>
+            </div>
+            <div className="input-data">
+              <input type="text" required />
+              <div className="underline" />
+              <label htmlFor="">Last Name</label>
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="input-data">
+              <input type="email" required />
+              <div className="underline" />
+              <label htmlFor="">Email Address</label>
+            </div>
+            <div className="input-data">
+              <input type="text" placeholder="" required />
+              <div className="underline" />
+              <label htmlFor="">Budget Range</label>
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="input-data textarea">
+              <textarea rows={8} cols={80} required defaultValue={""} />
+              <br />
+              <div className="underline" />
+              <label htmlFor="">Write your message</label>
+              <br />
+              <div className="submit-btn">
+                <div className="input-data">
+                  <div className="inner" />
+                  <div className="button">
+                    <input type="button" value="Submit" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -8,117 +8,116 @@ import "./navbar.css";
 
 // million-ignore
 export function NavbarDemo() {
-	return (
-		<div className="w-full flex items-center justify-center px-16 py-5 max-md:justify-start max-md:px-6 ">
-			<div className="flex items-center justify-center text-white font-extrabold leading-snug text-xl max-md:mt-6	" >
-				<Link href="/#">
-					<Image
-						src="/cypro4.webp"
-						alt=""
-						height={65}
-						width={65}
-						className="rounded-full mr-2"
-					/>
-				</Link>
-				<Link href="/#">
-					<div className="text leading-none">
-						<h1 className="bg-clip-text text-transparent font-[900] bg-gradient-to-r from-[#FF1D60] to-[#C71869]">
-							CYPRO
-						</h1>
-						<span className="">STUDIO</span>
-					</div>
-				</Link>
-			</div>
-			<Navbar className="top-8 max-md:hidden" />
-		
-			<Link
-				href="/contact"
-				className=" max-md:hidden redbtn bg-gradient-to-r from-[#FF6053] via-[#FF1D60] to-[#C71869] px-8 py-3 rounded-[3px] ">
-				<span className="spani text-white font-semibold text-xl">ORDER</span>
-			</Link>
+  return (
+    <div className="w-full flex items-center justify-center px-16 py-5 max-md:justify-start max-md:px-6 ">
+      <div className="flex items-center justify-center text-white font-extrabold leading-snug text-xl max-md:mt-6	">
+        <Link href="/#">
+          <Image
+            src="/cypro4.webp"
+            alt=""
+            height={65}
+            width={65}
+            className="rounded-full mr-2"
+          />
+        </Link>
+        <Link href="/#">
+          <div className="text leading-none">
+            <h1 className="bg-clip-text text-transparent font-[900] bg-gradient-to-r from-[#FF1D60] to-[#C71869]">
+              CYPRO
+            </h1>
+            <span className="">STUDIO</span>
+          </div>
+        </Link>
+      </div>
+      <Navbar className="top-8 max-md:hidden" />
 
-	<>
-		<input 
-			className="menu-icon  "
-			type="checkbox"
-			id="menu-icon"
-			name="menu-icon"
-		/>
-		<label htmlFor="menu-icon" />
-			<nav className="nav">
-				<ul className="pt-5">
-				<li>
-					<Link href="/about">About</Link>
-				</li>
-				<li>
-					<Link href="/about">Studio</Link>
-				</li>
-				<li>
-					<Link href="/about">Work</Link>
-				</li>
-				<li>
-					<Link href="/contact">Contact</Link>
-				</li>
-				</ul>
-			</nav>
-  </>
-		</div>
-	);
+      <Link
+        href="/contact"
+        className=" max-md:hidden redbtn bg-gradient-to-r from-[#FF6053] via-[#FF1D60] to-[#C71869] px-8 py-3 rounded-[3px] "
+      >
+        <span className="spani text-white font-semibold text-xl">ORDER</span>
+      </Link>
+
+      <>
+        <input
+          className="menu-icon"
+          type="checkbox"
+          id="menu-icon"
+          name="menu-icon"
+        />
+        <label htmlFor="menu-icon" />
+        <nav className="nav">
+          <ul className="pt-5">
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/about">Studio</Link>
+            </li>
+            <li>
+              <Link href="/about">Work</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </>
+    </div>
+  );
 }
 
 function Navbar({ className }: { className?: string }) {
-	const [active, setActive] = useState<string | null>(null);
-	return (
-		<div className={cn("inset-x-0 max-w-2xl mx-auto z-50", className)}>
-			<Menu setActive={setActive}>
-				<MenuItem setActive={setActive} active={active} item="About">
-					<div className="flex flex-col space-y-4 text-sm">
-						<HoveredLink href="/">Home</HoveredLink>
-						<HoveredLink href="/about">Blogs</HoveredLink>
-						<HoveredLink href="/about">About Cypro</HoveredLink>
-						<HoveredLink href="/about">Apply as Freelancer</HoveredLink>
-					</div>
-				</MenuItem>
-				<MenuItem setActive={setActive} active={active} item="Services">
-					<div className="flex flex-col space-y-4 text-sm">
-						<HoveredLink href="/about">Web Development</HoveredLink>
-						<HoveredLink href="/about">Interface Design</HoveredLink>
-						<HoveredLink href="/about">Search Engine Optimization</HoveredLink>
-						<HoveredLink href="/about">Branding</HoveredLink>
-					</div>
-				</MenuItem>
-				<MenuItem setActive={setActive} active={active} item="Products">
-					<div className="text-sm grid grid-cols-2 gap-10 p-4">
-						<ProductItem
-							title="Algochurn"
-							href="/"
-							src="/algochurn.webp"
-							description="Prepare for tech interviews like never before."
-						/>
-						<ProductItem
-							title="Tailwind Master Kit"
-							href="/"
-							src="/tailwindmasterkit.webp"
-							description="Production ready Tailwind css components for your next project"
-						/>
-						<ProductItem
-							title="Moonbeam"
-							href="/"
-							src="/screenshot.png"
-							description="Never write from scratch again. Go from idea to blog in minutes."
-						/>
-						<ProductItem
-							title="Rogue"
-							href="/"
-							src="/idk.png"
-							description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-						/>
-					</div>
-				</MenuItem>
-				<Link href="/contact">Contact</Link>
-			</Menu>
-
-
-		</div>
-	);
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div className={cn("inset-x-0 max-w-2xl mx-auto z-50", className)}>
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="About">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/">Home</HoveredLink>
+            <HoveredLink href="/about">Blogs</HoveredLink>
+            <HoveredLink href="/about">About Cypro</HoveredLink>
+            <HoveredLink href="/about">Apply as Freelancer</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Services">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/about">Web Development</HoveredLink>
+            <HoveredLink href="/about">Interface Design</HoveredLink>
+            <HoveredLink href="/about">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/about">Branding</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Products">
+          <div className="text-sm grid grid-cols-2 gap-10 p-4">
+            <ProductItem
+              title="Algochurn"
+              href="/"
+              src="/algochurn.webp"
+              description="Prepare for tech interviews like never before."
+            />
+            <ProductItem
+              title="Tailwind Master Kit"
+              href="/"
+              src="/tailwindmasterkit.webp"
+              description="Production ready Tailwind css components for your next project"
+            />
+            <ProductItem
+              title="Moonbeam"
+              href="/"
+              src="/screenshot.png"
+              description="Never write from scratch again. Go from idea to blog in minutes."
+            />
+            <ProductItem
+              title="Rogue"
+              href="/"
+              src="/idk.png"
+              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+            />
+          </div>
+        </MenuItem>
+        <Link href="/contact">Contact</Link>
+      </Menu>
+    </div>
+  );
 }
